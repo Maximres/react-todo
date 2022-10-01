9341886f4249f005ce6ad921e667f614ff06bcd2
Max <bulgachmaxim231@gmail.com>
2022-10-02 01:22:35 +0300

    sub tasks refactor

---
+++ src/features/details/components/rows/SubRowContextMenu.tsx
@@ -0,0 +1,42 @@
+﻿import { ClickEvent, Menu, MenuDivider, MenuItem } from "@szhsin/react-menu";
+import Icons from "@/components/AppIcons";
+import { SubItemOperations } from "../../ducks/constants/contextMenuOperations";
+import React from "react";
+
+type Props = {
+  isChecked: boolean;
+  onItemClick: (e: ClickEvent) => void;
+};
+
+const SubRowContextMenu = React.forwardRef(({ isChecked, onItemClick }: Props, ref: any) => (
+  <Menu
+    menuButton={
+      <div>
+        <Icons.Options className="px-2" />
+      </div>
+    }
+    boundingBoxRef={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      e.stopPropagation();
+    }}
+    onItemClick={onItemClick}
+  >
+    {isChecked && (
+      <MenuItem value={SubItemOperations.ToggleComplete}>Mark as not completed</MenuItem>
+    )}
+    {!isChecked && (
+      <>
+        <MenuItem value={SubItemOperations.ToggleComplete}>Mark as completed</MenuItem>
+        <MenuItem value={SubItemOperations.Promote}>Promote to task</MenuItem>
+      </>
+    )}
+
+    <MenuDivider />
+    <MenuItem value={SubItemOperations.Delete} className="text-danger">
+      Delete step
+    </MenuItem>
+  </Menu>
+));
+
+export { SubRowContextMenu };
--- src/features/details/components/rows/SubRows.tsx
+++ src/features/details/components/rows/SubRows.tsx
@@ -2,9 +2,8 @@
 import React, { useRef } from "react";
 import { SubRowCheckBox } from "@/features/details/components/rows/SubRowCheck";
 import { SubRowText } from "@/features/details/components/rows/SubRowText";
-import { ClickEvent, Menu, MenuDivider, MenuItem } from "@szhsin/react-menu";
-import Icons from "@/components/AppIcons";
-import { SubItemOperations } from "../../ducks/constants/contextMenuOperations";
+import { ClickEvent } from "@szhsin/react-menu";
+import { SubRowContextMenu } from "@/features/details/components/rows/SubRowContextMenu";
 
 type Props = {
   subTask: ISubTask;
@@ -14,6 +13,10 @@ type Props = {
 const SubRows = ({ subTask, onItemClick }: Props) => {
   const ref = useRef(null);
 
+  const onContextItemClick = (e: ClickEvent) => {
+    onItemClick(e, subTask);
+  };
+
   return (
     <div
       className="list-group-item d-flex justify-content-between align-items-center"
@@ -22,36 +25,7 @@ const SubRows = ({ subTask, onItemClick }: Props) => {
     >
       <SubRowCheckBox uid={subTask.id} isChecked={subTask.isChecked} />
       <SubRowText uid={subTask.id} text={subTask.text} isChecked={subTask.isChecked} />
-      <Menu
-        menuButton={
-          <div>
-            <Icons.Options className="px-2" />
-          </div>
-        }
-        boundingBoxRef={ref}
-        onClick={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-        }}
-        onItemClick={(e) => {
-          onItemClick(e, subTask);
-        }}
-      >
-        {subTask.isChecked && (
-          <MenuItem value={SubItemOperations.ToggleComplete}>Mark as not completed</MenuItem>
-        )}
-        {!subTask.isChecked && (
-          <>
-            <MenuItem value={SubItemOperations.ToggleComplete}>Mark as completed</MenuItem>
-            <MenuItem value={SubItemOperations.Promote}>Promote to task</MenuItem>
-          </>
-        )}
-
-        <MenuDivider />
-        <MenuItem value={SubItemOperations.Delete} className="text-danger">
-          Delete step
-        </MenuItem>
-      </Menu>
+      <SubRowContextMenu ref={ref} isChecked={subTask.isChecked} onItemClick={onContextItemClick} />
     </div>
   );
 };
